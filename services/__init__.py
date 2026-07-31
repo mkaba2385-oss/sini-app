92a03cf3d62752662d2bd3f3735dc3b71ded48a2
Moussa Kaba <mkaba2385@gmail.com>
2026-07-31 19:29:28 +0200

    feat(services): exporter ParcelleService et les exceptions dans le init

---
+++ services/__init__.py
@@ -0,0 +1,8 @@
+from .parcelle_service import ParcelleService
+from .exceptions import SiniServiceError, EntityNotFoundError
+
+__all__ = [
+    "ParcelleService",
+    "SiniServiceError",
+    "EntityNotFoundError",
+]
