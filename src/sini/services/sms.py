b426057a160a9c5b7afab240bafb0796fa40e680
Moussa Kaba <mkaba2385@gmail.com>
2026-08-05 19:44:55 +0200

    feat: ajouter sms.py

---
+++ src/sini/services/sms.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+
+class SmsGateway(ABC):
+    @abstractmethod
+    def send_sms(self, telephone: str, message: str) -> bool:
+        pass
+
+class ConsoleSmsGateway(SmsGateway):
+    def send_sms(self, telephone: str, message: str) -> bool:
+        print(f"[SMS STUB -> {telephone}] : {message}")
+        return True
