1f3333034482c4ea3b9c00d36e641d7aef2bd3a4
Moussa Kaba <mkaba2385@gmail.com>
2026-08-15 17:07:27 +0200

    test(fixtures): corriger les fixtures de test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,9 @@
 import pytest
+
 from sini.factories.service_factory import ServiceFactory
-from sini.observers.base import EventPublisher
-from sini.repositories.memory import InMemoryParcelleRepository
 from sini.services.parcelle_service import ParcelleService
-from sini.services.sms import ConsoleSmsGateway  
-from sini.services.weather import MockWeatherProvider
 
 
 @pytest.fixture
 def parcelle_service() -> ParcelleService:
-    return ServiceFactory.create_parcelle_service(env="dev")  
+    return ServiceFactory.create_parcelle_service(env="dev")
