bf73c106e31faad46a54734c0bf868db258c791e
Moussa Kaba <mkaba2385@gmail.com>
2026-09-22 16:21:38 +0200

    feat: ajouter le docker compose

--- backend/main.py
+++ backend/main.py
@@ -25,6 +25,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://localhost:4173",
         "http://127.0.0.1:5173",
+        "http://localhost:8081",
         "https://sini-frontend.vercel.app",
     ],
     allow_credentials=True,
